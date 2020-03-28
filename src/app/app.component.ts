407f485698eaa2c19f487b368cbbf2bfd454f540
elkolotfi <elkolotfi@gmail.com>
2020-03-28 09:46:11 +0100

    add ga

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+// tslint:disable-next-line:ban-types
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,12 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'UA-92509401-4', { page_path: event.urlAfterRedirects });
+      }
+    });
+  }
 }
